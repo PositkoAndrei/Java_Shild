282d34a84111795dd63fb84792bea30ddbc8db27
Andrey <positko.andrei@gmail.com>
2019-08-15 09:24:49 +0300

    ForIf

---
+++ src/BlockDemo.java
@@ -0,0 +1,14 @@
+public class BlockDemo {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        a = 5;
+        b = 10;
+
+        if (a != 0) {
+            System.out.println("а не равно нулю");
+            c = b / a;
+            System.out.println("b / a равно: " + c);
+        }
+    }
+}
