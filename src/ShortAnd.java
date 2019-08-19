9940e6e0d96abbd9a1aad58a3296c9bf191ab91e
Andrey <positko.andrei@gmail.com>
2019-08-19 20:06:20 +0300

    SideEffect

---
+++ src/ShortAnd.java
@@ -0,0 +1,18 @@
+public class ShortAnd {
+    public static void main(String[] args) {
+        int n, d, q;
+
+        n = 10;
+        d = 2;
+        if (d != 0 && (n % d) == 0)
+            System.out.println(d + " является делителем " + n);
+
+        d = 0;
+
+        if (d != 0 && (n % d) == 0)
+            System.out.println(d + " является делителем " + n);
+
+        if (d != 0 & (n % d) == 0)
+            System.out.println(d + " является делителем " + n);
+    }
+}
