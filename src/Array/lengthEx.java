542cdab8c09a600e63db9dd1d90f41e1adb3f1bc
Andrey <positko.andrei@gmail.com>
2019-08-23 18:15:47 +0300

    ForEach

--- src/Array/lengthEx.java
+++ src/Array/lengthEx.java
@@ -5,7 +5,7 @@ public class lengthEx {
         int[] list = new int[10];
         int[] nums = {1, 2, 3};
         int[][] table = {
-                {1, 2, 3},
+                {1, 2, 3} ,
                 {4, 5},
                 {6, 7, 8, 9}
         };
---
+++ src/ForEach/ForEachSearch.java
@@ -0,0 +1,18 @@
+package ForEach;
+
+public class ForEachSearch {
+    public static void main(String[] args) {
+        int[] nums = {6 , 8, 3, 7, 5, 6, 1, 4};
+        int val = 5;
+        boolean found = false;
+
+        for (int x : nums){
+            if (x == val){
+                found = true;
+                break;
+            }
+        }
+        if (found)
+            System.out.println("Значение найдено!)" + val);
+    }
+}
