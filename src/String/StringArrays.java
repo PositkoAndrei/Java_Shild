409fca0245cfe7942423bdf15b5f8d3caa6bff14
Andrey <positko.andrei@gmail.com>
2019-08-26 11:46:20 +0300

    String2

---
+++ src/String/StringArrays.java
@@ -0,0 +1,22 @@
+package String;
+
+public class StringArrays {
+    public static void main(String[] args) {
+        String[] strs = {"Эта", "строка", "является", "текстом"};
+
+        System.out.println("Исходный массив: ");
+        for (String s : strs) {
+            System.out.print(s + " ");
+        }
+        System.out.println("\n");
+
+        //изменить  строку
+        strs[2] = "также является";
+        strs[3] = "Текстом!";
+
+        System.out.println("Измененный массив: ");
+        for (String s : strs) {
+            System.out.print(s + " ");
+        }
+    }
+}
